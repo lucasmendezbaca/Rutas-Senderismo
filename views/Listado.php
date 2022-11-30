874faac58bf7c138c4f559b73eb54500a7d04b37
Lucas Mendez <lucasmendezbaca@gmail.com>
2022-11-30 14:16:28 +0100

    tabla arreglada

--- views/Listado.php
+++ views/Listado.php
@@ -1,12 +1,12 @@
 <table>
-    <th>
+    <tr>
         <td>Titulo</td>
         <td>Descripción</td>
         <td>Desnivel (m)</td>
         <td>Distancia (km)</td>
         <td>Dificultad</td>
         <td>Operaciones</td>
-    </th>
+    </tr>
     <?php
     foreach($rutas as $ruta) {
         echo "<tr>";
@@ -15,8 +15,10 @@
             echo "<td>" . $ruta->getDesnivel() . "</td>";
             echo "<td>" . $ruta->getDistancia() . "</td>";
             echo "<td>" . $ruta->getDificultad() . "</td>";
-            echo "<td><a href='index.php?controller=ruta&action=edit&id=" . $ruta->getId() . "'>Editar</a></td>";
-            echo "<td><a href='index.php?controller=ruta&action=delete&id=" . $ruta->getId() . "'>Borrar</a></td>";
+            echo "<td>";
+                echo "<a href='index.php?controller=ruta&action=edit&id=" . $ruta->getId() . "'>Editar</a>";
+                echo "<a href='index.php?controller=ruta&action=delete&id=" . $ruta->getId() . "'>Borrar</a>";
+            echo "</td>";
         echo "</tr>";
     }
     ?>
